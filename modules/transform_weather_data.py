9b621440770d58e432549ab94490d53638303840
rodrigobmedeiros <rodrigo.medeiros@radixeng.com.br>
2021-01-24 17:55:44 -0300

    Include a specific function to filter weather data coming from open weather api

---
+++ modules/transform_weather_data.py
@@ -0,0 +1,13 @@
+def filter_weather_data(weather_info):
+    """
+    This function is responsible to filter weather data obtained from open weather api and
+    returns just city_id, temperature and humidity.
+    """
+
+    filtered_weather_data = {
+        'city_id': weather_info['id'],
+        'temperature_celsius':  round(weather_info['main']['temp'] - 273.15, 2),
+        'humidity': weather_info['main']['humidity']
+    }
+
+    return filtered_weather_data
