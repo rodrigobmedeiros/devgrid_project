99cd19141bafb55a88e4c763b7df8dc023932c70
rodrigobmedeiros <rodrigo.medeiros@radixeng.com.br>
2021-01-24 17:56:36 -0300

    include a function to GET filtered weather data information

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from flask import Flask, jsonify
 from modules.set_config import SetConfig
+from modules.transform_weather_data import filter_weather_data
 import os
+import requests
 
 def create_app():
 
     app = Flask('__name__')
 
     open_weather_api_config = SetConfig()
-    print(open_weather_api_config.url_weather_api)
-    print(open_weather_api_config.x_rapidapi_key)
-    print(open_weather_api_config.x_rapidapi_host)
-
 
     @app.route('/')
     def test_endpoint():
-
         
         return jsonify({'name': 'Rodrigo'})
 
 
+    @app.route('/<city_id>', methods=['GET'])
+    def weather_data(city_id):
 
+        querystring = {"id": city_id}
 
+        response = requests.request("GET",
+                                    open_weather_api_config.url_weather_api,
+                                    headers=open_weather_api_config.headers,
+                                    params=querystring)
 
+        transformed_weather_data = filter_weather_data(response.json())
 
+        return jsonify(transformed_weather_data)
 
     return app
